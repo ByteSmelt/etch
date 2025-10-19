0149dfb8108f2b9b554f5b0f2aec227b288c814e
kunitoki <kunitoki@gmail.com>
2025-10-19 16:11:00 +0200

    Export functions from dll (windows)

--- examples/clib/mathlib.c
+++ examples/clib/mathlib.c
@@ -2,16 +2,28 @@
 
 #include <stdint.h>
 
-// Simple integer operations
-int64_t c_add(int64_t a, int64_t b) {
+// Platform-specific DLL export macro
+#ifdef _WIN32
+    #define MATHLIB_EXPORT __declspec(dllexport)
+#elif defined(__GNUC__) && __GNUC__ >= 4
+    #define MATHLIB_EXPORT __attribute__((visibility("default")))
+#else
+    #define MATHLIB_EXPORT
+#endif
+
+MATHLIB_EXPORT int64_t c_abs(int64_t x) {
+    return x < 0 ? -x : x;
+}
+
+MATHLIB_EXPORT int64_t c_add(int64_t a, int64_t b) {
     return a + b;
 }
 
-int64_t c_multiply(int64_t a, int64_t b) {
+MATHLIB_EXPORT int64_t c_multiply(int64_t a, int64_t b) {
     return a * b;
 }
 
-int64_t c_power(int64_t base, int64_t exp) {
+MATHLIB_EXPORT int64_t c_power(int64_t base, int64_t exp) {
     int64_t result = 1;
     for (int64_t i = 0; i < exp; i++) {
         result *= base;
@@ -19,17 +31,11 @@ int64_t c_power(int64_t base, int64_t exp) {
     return result;
 }
 
-// Simple factorial function
-int64_t c_factorial(int64_t n) {
+MATHLIB_EXPORT int64_t c_factorial(int64_t n) {
     if (n <= 1) return 1;
     int64_t result = 1;
     for (int64_t i = 2; i <= n; i++) {
         result *= i;
     }
     return result;
-}
-
-// Simple abs function
-int64_t c_abs(int64_t x) {
-    return x < 0 ? -x : x;
 }
